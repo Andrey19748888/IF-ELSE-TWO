1749871cb0e7aff2b20533e16f00327de7ea6b4c
Andreydenisov7788 <asasa88881974@mail.ru>
2022-09-06 19:52:35 +0300

    задание 1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int clientos = 1;
+        if (clientos == 0) {
+            System.out.println("Установите версию приложения IOS по ссылке"); }
+        else if (clientos == 1) {
+            System.out.println("Установите версию приложения Android по ссылке");
+
+        }
+
+    }
+}
