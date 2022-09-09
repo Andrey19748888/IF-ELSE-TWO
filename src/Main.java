76093d26d3f83fc8621ea63395479958a286fc73
Andreydenisov7788 <asasa88881974@mail.ru>
2022-09-09 23:42:08 +0300

    задание 4

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int clientos = 1;
-        if (clientos == 0) {
-            System.out.println("Установите версию приложения IOS по ссылке"); }
-        else if (clientos == 1) {
-            System.out.println("Установите версию приложения Android по ссылке");
+        int deliveryDistance = 20;
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: 1");
+        }
+        else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            System.out.println("Потребуется дней: 2");
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            System.out.println("Потребуется дней: 3");
 
         }
 
